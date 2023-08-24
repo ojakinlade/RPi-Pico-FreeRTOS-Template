74bac34ee64a32db301081c84bece04538ca2c36
JosephAkinlade <josebanky11@gmail.com>
2023-08-24 17:35:48 +0100

    update

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "FreeRTOS.h"
+#include "FreeRTOSConfig.h"
+#include "task.h"
+
+TaskHandle_t blinkTaskHandle = NULL;
+
+void BlinkTask(void* pvParameters)
+{  
+    for(;;)  
+    {
+        gpio_put(PICO_DEFAULT_LED_PIN, true);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        gpio_put(PICO_DEFAULT_LED_PIN, false);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    } 
+}
+
+int main(void)
+{
+    stdio_init_all();
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+
+    xTaskCreate(BlinkTask, "BlinkTask", 256, NULL, 1, &blinkTaskHandle);
+    vTaskStartScheduler();
+
+    while(true)
+    {
+
+    }
+}
